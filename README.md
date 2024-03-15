b4c654f99b31eb922b675da921bc80e53e9217b1
Fillsogood <jdd05273@naver.com>
2024-03-15 16:42:29 +0900

    mysql 연동

--- README.md
+++ README.md
@@ -1,10 +1,71 @@
 ## portfoilo-site
-- 개인 포트폴리오 사이트 만들기
+- 개인 포트폴리오 사이트 만들어서 서버 배포까지 목표
 ### venv-setting
 - python3 -m venv .venv
 - source .venv/bin/activate
 
 ### django-setting
 - pip install django
-- django-admin startproject config # 현재 폴더에서 프로젝트 생성
+- django-admin startproject config . # 현재 폴더에서 프로젝트 생성
+- python manage.py startapp users
+- python manage.py startapp portfoilo
+- python manage.py startapp common
 
+### mysql 연동
+- pip install mysqlclient
+- db.py 만들어서 프라이빗하게 하고 settings.py에 설정
+- 모델 정의 후
+- python manage.py inspectdb 
+
+### 모델 구조
+1. User
+
+- email: str unique 
+- Nicname: str
+- password: str
+
+2. portfolio
+- title: str
+- description: str
+- image: image
+- link: str
+- user_id: int FK
+
+3. common
+- create_at
+- update_at
+
+### Rest API
+
+(1) User API
+
+login
+api/v1/login
+- POST: 로그인
+
+logout
+api/v1/logout
+- POST: 로그아웃
+
+signup
+api/v1/signup
+- POST: 회원가입
+
+UserDetaiview
+- GET: 특정 유저 조회
+
+MyInfo
+- GET: 자신 정보 조회
+- PUT: 자신 정보 수정
+
+(2) Portfoilo_post API
+
+PortfolioList
+api/v1/feeds
+- POST: 새로운 포트폴리오 생성
+- GET: 전체 목록 조회
+
+PortfolioDetail
+- GET: 특정 포폴 상세 조회
+- PUT: 특정 포폴 정보 수정
+- DELETE: 특정 포폴 삭제
--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import db_settings
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qb!!49qon$m3a0-zsf*g0!&87188^44dem91=^u@18^ue44hj#'
+SECRET_KEY = db_settings.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -29,6 +29,19 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
+DJANGO_SYSTEM_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+CUSTOM_USER_APPS = [
+    'users.apps.UsersConfig',
+    'portfolios.apps.PortfoliosConfig',
+    'common.apps.CommonConfig',
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -37,6 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
 ]
 
 MIDDLEWARE = [
@@ -73,12 +87,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = db_settings.DATABAES
+
 
 
 # Password validation
